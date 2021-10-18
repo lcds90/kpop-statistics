08fa77b3c5663f789ba425eebc6e5d1071082f8a
Leonardo Santos <lcds90@gmail.com>
2021-10-17 23:14:28 -0300

    eslint disable in build

--- next.config.js
+++ next.config.js
@@ -6,4 +6,9 @@ module.exports = {
   env: {
     YOUTUBE_API: process.env.YOUTUBE_API,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
