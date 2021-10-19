a3d7fbd19cee8b0ac7b224a526fb3cdbe576df2f
Leonardo Santos <lcds90@gmail.com>
2021-10-19 05:37:40 -0300

    refactoring in helpers

---
+++ pages/playlist/[id]/Music/helpers.js
@@ -0,0 +1,12 @@
+const capitalizeName = (name) => name.charAt(0).toUpperCase() + name.slice(1);
+export const labelFormatterPercentage = ({ value }) => `${(value)}%`;
+export const labelFormatterTime = ({ value }) => `${(value)}s`;
+export const timeStatistics = (statistics) => Object
+  .entries(statistics)
+  .filter((s) => s[0] !== 'id')
+  .map((s) => ({ name: capitalizeName(s[0]), value: s[1].time, fill: s[1].fill }));
+
+export const percentageStatistics = (statistics) => Object
+  .entries(statistics)
+  .filter((s) => s[0] !== 'id')
+  .map((s) => ({ name: capitalizeName(s[0]), value: s[1].percentage, fill: s[1].fill }));
--- pages/playlist/[id]/Music/index.jsx
+++ pages/playlist/[id]/Music/index.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 import styles from 'styles/Playlist.module.css';
 import {
-  PieChart, Pie, Label, Legend, Tooltip, ResponsiveContainer,
+  PieChart, Pie, Legend, Tooltip, ResponsiveContainer,
 } from 'recharts';
+import {
+  labelFormatterTime, labelFormatterPercentage,
+  percentageStatistics, timeStatistics,
+} from './helpers';
 
 const Music = ({
   music: {
@@ -18,61 +22,47 @@ const Music = ({
     },
     statistics,
   },
-}) => {
-  const capitalizeName = (name) => name.charAt(0).toUpperCase() + name.slice(1);
-  const timeStatistics = Object
-    .entries(statistics)
-    .filter((s) => s[0] !== 'id')
-    .map((s) => ({ name: capitalizeName(s[0]), value: s[1].time, fill: s[1].fill }));
-  const percentageStatistics = Object
-    .entries(statistics)
-    .filter((s) => s[0] !== 'id')
-    .map((s) => ({ name: capitalizeName(s[0]), value: s[1].percentage, fill: s[1].fill }));
-  const labelFormatterPercentage = ({ value }) => `${(value)}%`;
-  const labelFormatterTime = ({ value }) => `${(value)}s`;
-  return (
-    <section className={styles.section}>
-      <article>
-        <h3>{title}</h3>
-        <div>
-          <ResponsiveContainer width="99%" height={300}>
-            <PieChart width={300} height={300}>
-              <Pie
-                dataKey="value"
-                isAnimationActive={false}
-                data={timeStatistics}
-                cx="50%"
-                cy="50%"
-                outerRadius={80}
-                label
-                legendType="square"
-                label={labelFormatterTime}
-              />
-              <Legend verticalAlign="top" height={36} />
-              <Tooltip />
-            </PieChart>
-          </ResponsiveContainer>
-          <ResponsiveContainer width="99%" height={300}>
+}) => (
+  <section className={styles.section}>
+    <article>
+      <h3>{title}</h3>
+      <div>
+        <ResponsiveContainer width="99%" height={300}>
+          <PieChart width={300} height={300}>
+            <Pie
+              dataKey="value"
+              isAnimationActive={false}
+              data={timeStatistics(statistics)}
+              cx="50%"
+              cy="50%"
+              outerRadius={80}
+              legendType="square"
+              label={labelFormatterTime}
+            />
+            <Legend verticalAlign="top" height={36} />
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
+        <ResponsiveContainer width="99%" height={300}>
 
-            <PieChart width={300} height={300}>
-              <Pie
-                dataKey="value"
-                isAnimationActive={false}
-                data={percentageStatistics}
-                cx="50%"
-                cy="50%"
-                outerRadius={80}
-                label={labelFormatterPercentage}
-              />
-              <Legend verticalAlign="top" height={36} />
-              <Tooltip />
-            </PieChart>
-          </ResponsiveContainer>
-        </div>
-      </article>
-      <ReactPlayer url={`https://www.youtube.com/watch?v=${videoId}`} controls />
-    </section>
-  );
-};
+          <PieChart width={300} height={300}>
+            <Pie
+              dataKey="value"
+              isAnimationActive={false}
+              data={percentageStatistics(statistics)}
+              cx="50%"
+              cy="50%"
+              outerRadius={80}
+              label={labelFormatterPercentage}
+            />
+            <Legend verticalAlign="top" height={36} />
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+    </article>
+    <ReactPlayer url={`https://www.youtube.com/watch?v=${videoId}`} controls />
+  </section>
+);
 
 export default Music;
