0e281bfa31098d6adfc8950118cf09832b41b08b
Leonardo Santos <lcds90@gmail.com>
2021-10-23 21:04:05 -0300

    fixing router

--- next.config.js
+++ next.config.js
@@ -8,15 +8,6 @@ module.exports = {
   env: {
     YOUTUBE_API: process.env.YOUTUBE_API,
   },
-  /*   async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true,
-      },
-    ];
-  }, */
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
